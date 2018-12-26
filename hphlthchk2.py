fc718bdb4a6f0e3c6ca6d02f80b0731ea6675c7a
jmedina6 <42040857+jmedina6@users.noreply.github.com>
2018-12-26 17:55:50 -0600

    SMTP Version

--- hphlthchk2.py
+++ hphlthchk2.py
@@ -5,12 +5,12 @@ import getpass
 import string
 import time
 import os
-import select
-import paramiko
-import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 print('\n')
-ip = input("IP Address: ")
+ip = input("IP Address: \b" )
 print('\n')
 username = input("Username: ")
 password = getpass.getpass()
@@ -37,8 +37,9 @@ if not os.path.exists(os.path.join(username, ticket)):
 
 
 print('\n#### HP Access-Switch Health Check ####\n')
+print('\n#### Incident\Task#: ####\n')
 print(ticket)
-print('\n')
+print('\n#### User ID: ####\n')
 print(username)
 
 print('\n#### Device hostname ####\n')
@@ -52,7 +53,12 @@ print(show_clock)
 
 print('\n#### Display logbuffer data ####\n')
 show_logbuffer = connection.send_command('display logbuffer')
+time.sleep(2)
 print(show_logbuffer)
+
+print('\n#### Display logbuffer data ####\n')
+show_neigh = connection.send_command('display lldp neighbor list')
+print(show_neigh)
 
 print('\n#### Display information center data ####\n')
 show_infocenter = connection.send_command('display info-center')
@@ -106,18 +112,66 @@ print('\n#### Clock status and configuration information ####\n')
 show_clock = connection.send_command('display clock')
 print(show_clock)
 
-with open(os.path.join(evidence_path, username, ticket, hostname + "-hlthchk.txt"), 'w') as f:
+with open(os.path.join(username, ticket, hostname + "-hlthchk.txt"), 'w') as f:
     f.write('\r'.join('#### HP Access-Switch Health Check ####\n\n\n\n'))
+    f.write('\r'.join('#### Incident\Task#: ####\n\n'))
+    f.write('\r'.join(ticket + '\n\n'))
+    f.write('\r'.join('#### User ID: ####: \n\n'))
+    f.write('\r'.join(username + '\n\n'))
     f.write('\r'.join('#### Device hostname ####\n\n\n\n'))
     f.write('\r'.join(hostname + '\n\n\n\n\n'))
     f.write('\r'.join('#### Display Time ####\n\n'))
     f.write('\r'.join(show_clock + '\n\n\n\n\n'))
     f.write('\r'.join('#### Display logbuffer data ####\n\n'))
     f.write('\r'.join(show_logbuffer + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display neighbor data ####\n\n'))
+    f.write('\r'.join(show_neigh + '\n\n\n\n\n'))
     f.write('\r'.join('#### Display information center data ####\n\n'))
     f.write('\r'.join(show_infocenter + '\n\n\n\n\n'))
     f.write('\r'.join('#### Display Manufacture information ####\n\n'))
     f.write('\r'.join(show_manuinfo + '\n\n\n\n\n'))
     f.write('\r'.join('#### Display inbound traffic counters ####\n\n'))
     f.write('\r'.join(show_incounters + '\n\n\n\n\n'))
-    f.close()
+    f.write('\r'.join('#### Display outbound traffic counters ####\n\n'))
+    f.write('\r'.join(show_outcounters + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display interface packet drops ####\n\n'))
+    f.write('\r'.join(show_packdrops + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display memory information ####\n\n'))
+    f.write('\r'.join(show_memory + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display cpu usage information ####\n\n'))
+    f.write('\r'.join(show_cpu + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display cpu usage history information ####\n\n'))
+    f.write('\r'.join(show_cpuhist + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display inbound traffic rate counters ####\n\n'))
+    f.write('\r'.join(show_inrate + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display outbound traffic rate counters ####\n\n'))
+    f.write('\r'.join(show_outrate + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display traffic behavior data ####\n\n'))
+    f.write('\r'.join(show_bhdefined + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display traffic classifier data ####\n\n'))
+    f.write('\r'.join(show_cldefined + '\n\n\n\n\n'))
+    f.write('\r'.join('#### Display Time ####\n\n'))
+    f.write('\r'.join(show_clock + '\n\n\n\n\n'))
+    f.write('\r'.join('#### END ####\n\n'))
+    exit()
+
+
+sender = ((username))@aa.com
+receivers = ((username))@aa.com
+
+msg = MIMEMultipart()
+msg['Subject'] = 'Test'
+msg['From'] = 'Jonas Medina'
+msg['To'] = username@aa.com
+file='-hlthchk.txt'
+
+msg.attach(MIMEText("Labour"))
+attachment = MIMEBase('ticket', '-hlthchk.txt')
+attachment.set_payload(open(ticket + "-hlthchk.txt", 'rb').read())
+encoders.encode_base64(attachment)
+attachment.add_header('Content-Disposition', 'attachment; filename="-hlthchk.txt"' % os.path.basename(file))
+msg.attach(attachment)
+
+print('Send email.')
+conn.sendmail(sender, receivers, msg.as_string())
+conn.close()
